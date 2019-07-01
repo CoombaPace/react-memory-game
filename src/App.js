e8fe2ff967abbf6474f05387455f2e5a11d17e5e
Coomba <coombapace@gmail.com>
2019-07-01 19:58:25 -0400

    added images from the game screen to give it that feel

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 import { Line } from 'rc-progress';
 import './App.css';
 import Wrapper from "./components/Wrapper";
-import BackgroundSlider from './components/BackGroundSlider/BackgroundSlider.js'
 import meChars from "./meChars.json";
+import Grid from '@material-ui/core/Grid';
+import GridList from '@material-ui/core/GridList';
 import CharCard from "./components/CharCard/CharCard.js";
+import BackgroundSlider from './components/BackGroundSlider/BackgroundSlider.js'
 import UI_Display_bottom from './UI_Display_bottom.jpeg'
 import UI_Display_left from './UI_Display_left.jpeg'
 import UI_Displa_right from './UI_Displa_right.jpeg'
+import UI_weapons from './UI_Display_weapons.jpeg'
+import UI_map from './UI_Display_map.jpeg'
 import image1 from './images/shepardsFistBump.jpg'
 import image2 from './images/chars.jpg'
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Grid';
-import GridList from '@material-ui/core/GridList';
+
 
 
 
@@ -79,12 +81,13 @@ class App extends Component {
     return (
 		
       <Wrapper>
-		  <Grid container spacing={12} id="mainGrid">
-        <Grid item xs>
+
+		  <Grid container spacing={4} id="mainGrid">
+        	<Grid item xs>
 			  <img className="leftSide" src={UI_Display_left} alt="headup_display" />
-        </Grid>
-        <GridList cols={3}>
-          
+        	</Grid>
+
+       	 <GridList cols={3}>
 			{this.state.meChars.map(meChar => (
 			<CharCard
 				setClicked={this.setClicked}
@@ -93,25 +96,40 @@ class App extends Component {
 				image={meChar.image}
 				name={meChar.name}
 			/>
-		))}
-		  
+			))} 
         </GridList>
         <Grid item xs>
 		  <img className="rightSide" src={UI_Displa_right} alt="headup_display" />;
         </Grid>
       </Grid>
-        
-
-		<div className="bottomComponents">
+	  <Grid item xs={3}>
 			<Line 
 				className="progress-bar"
 				percent={this.state.guessesCorrect}
 				trailWidth="12" 
 				strokeWidth="12" 
 				strokeColor="#9ABCC2"
-				strokeLinecap="square" />
-			<img className="bottom" src={UI_Display_bottom} alt="headup_display" />
-		</div>
+				strokeLinecap="square" 
+			/>
+		</Grid>
+	  <Grid container spacing={3}
+			direction="row"
+			justify="center"
+			alignItems="center">
+
+		
+			<Grid item xs>
+				<img className="weapons" src={UI_weapons} alt="headup_display" />;
+        	</Grid>
+			
+			<Grid item xs={6}>
+				<img className="bottom" src={UI_Display_bottom} alt="headup_display" />
+			</Grid>
+
+			<Grid item xs>
+				<img className="map" src={UI_map} alt="headup_display" />;
+			</Grid>
+		</Grid>
 		<BackgroundSlider
 			images={[image1, image2]}
 			duration={10} transition={2} />
